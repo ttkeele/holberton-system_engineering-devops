1db9d669d867fd612038d8f8b77acb7bce4449c9
ttkeele <3264@holbertonschool.com>
2022-01-04 21:43:06 +0000

    fixed indent

--- 0x0E-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         for task in tasks:
             t_tasks[user_id].append({"username": username,
                                     "task": task.get('title'),
-                                    "completed": task.get('completed')})
+                                     "completed": task.get('completed')})
 
     with open('todo_all_employees.json', 'w') as jsonfile:
         json.dump(t_tasks, jsonfile)
