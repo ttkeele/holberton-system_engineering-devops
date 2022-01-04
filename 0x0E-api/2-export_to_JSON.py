eb510943e10e39e2744ce3ec16a75f83c41fdf3f
ttkeele <3264@holbertonschool.com>
2022-01-04 21:22:41 +0000

    wrong ids

--- 0x0E-api/2-export_to_JSON.py
+++ 0x0E-api/2-export_to_JSON.py
@@ -6,21 +6,21 @@ import json
 
 
 if __name__ == "__main__":
-    userId = argv[1]
-    user = requests.get(
+    user_id = argv[1]
+    user_info = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}'.
-        format(userId)).json()
-    username = user.get('username')
+        format(user_id)).json()
+    username = user_info.get('username')
     tasks = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}/todos'.
-        format(userId)).json()
+        format(user_id)).json()
 
-    t_tasks = {userId: []}
+    t_tasks = {user_id: []}
 
     for task in tasks:
-        t_tasks[userId].append({"task": task.get('title'),
+        t_tasks[user_id].append({"task": task.get('title'),
                                 "completed": task.get('completed'),
                                 "username": username})
 
-    with open('{}.json'.format(userId), 'w') as jsonfile:
+    with open('{}.json'.format(user_id), 'w') as jsonfile:
         json.dump(t_tasks, jsonfile)
